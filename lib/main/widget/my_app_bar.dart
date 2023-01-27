44ec6235912468761a9cab61a5e8b33be40f6bb9
commandiron <commandiron@gmail.com>
2023-01-27 18:04:25 +0300

    .

--- lib/main/widget/my_app_bar.dart
+++ lib/main/widget/my_app_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:virtual_showroom/configs/app_space.dart';
 import 'package:virtual_showroom/configs/app_text_style.dart';
 import 'package:virtual_showroom/helper/url_launcher.dart';
 import 'package:virtual_showroom/model/company_contact.dart';
@@ -40,64 +39,36 @@ class MyAppBar extends StatelessWidget {
                         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
                         title: Text("İletişim", style: AppTextStyle.h3!.copyWith(color: Colors.black),),
                         contentPadding: AppPadding.horizontalL!.add(AppPadding.verticalM!),
-                        children: [
-                          Text("Telefon", style: AppTextStyle.b1b!.copyWith(color: Colors.black),),
-                          Row(
+                        children: CompanyContact.items.map(
+                          (item) => Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
                             children: [
-                              Text(CompanyContact.item.phoneNumber, style: AppTextStyle.b2!.copyWith(color: Colors.black),),
-                              IconButton(
-                                onPressed: () {
-                                  openURL("tel:${CompanyContact.item.phoneNumber}");
-                                },
-                                icon: Icon(
-                                  Icons.phone,
-                                  color: Theme.of(context).colorScheme.primary
-                                )
-                              )
-                            ],
-                          ),
-                          AppSpace.verticalS!,
-                          Text("E posta", style: AppTextStyle.b1b!.copyWith(color: Colors.black),),
-                          Row(
-                            children: [
-                              Text(CompanyContact.item.email, style: AppTextStyle.b2!.copyWith(color: Colors.black),),
-                              IconButton(
-                                  onPressed: () {
-                                    openURL("mailto:${CompanyContact.item.email}");
-                                  },
-                                  icon: Icon(
-                                    Icons.mail,
-                                    color: Theme.of(context).colorScheme.primary
-                                  )
-                              )
-                            ],
-                          ),
-                          AppSpace.verticalS!,
-                          Text("Ofis", style: AppTextStyle.b1b!.copyWith(color: Colors.black),),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Expanded(
-                                flex: 4,
+                              Align(
+                                alignment: Alignment.centerLeft,
                                 child: Text(
-                                  CompanyContact.item.address,
-                                  style: AppTextStyle.b2!.copyWith(color: Colors.black),
+                                  item.title,
+                                  style: AppTextStyle.b1b!.copyWith(color: Colors.black),
                                 )
                               ),
-                              Expanded(
-                                child: IconButton(
-                                  onPressed: () {
-                                    openURL("https://goo.gl/maps/RNiqv558Wzu3nqeR7");
-                                  },
-                                  icon: Icon(
-                                    Icons.location_on,
-                                    color: Theme.of(context).colorScheme.primary
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      item.body,
+                                      style: AppTextStyle.b2!.copyWith(color: Colors.black),
+                                    )
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      openURL(item.url);
+                                    },
+                                    icon: Icon(item.icon, color: Theme.of(context).colorScheme.primary,)
                                   )
-                                ),
-                              )
+                                ],
+                              ),
                             ],
-                          ),
-                        ],
+                          )
+                        ).toList()
                       );
                     },
                   );
--- lib/model/company_contact.dart
+++ lib/model/company_contact.dart
@@ -1,21 +1,38 @@
-
+import 'package:flutter/material.dart';
 
 class CompanyContact {
-  String phoneNumber;
-  String email;
-  String address;
+  String title;
+  String body;
+  String url;
+  IconData icon;
 
   CompanyContact(
     {
-      required this.phoneNumber,
-      required this.email,
-      required this.address,
+      required this.title,
+      required this.body,
+      required this.url,
+      required this.icon,
     }
   );
 
-  static CompanyContact item = CompanyContact(
-    phoneNumber: "+902163300300",
-    email: "info@demirli.com.tr",
-    address: "Yıldızbakkal Taşköprü Cad. Demirli İş Merkezi Kadıköy - İSTANBUL"
-  );
+  static List<CompanyContact> items = [
+    CompanyContact(
+      title: "Telefon",
+      body: "0216 330 03 00",
+      url: "tel:+902163300300",
+      icon: Icons.phone,
+    ),
+    CompanyContact(
+      title: "E posta",
+      body: "info@demirli.com.tr",
+      url: "mailto:info@demirli.com.tr",
+      icon: Icons.mail,
+    ),
+    CompanyContact(
+      title: "Ofis",
+      body: "Yıldızbakkal Taşköprü Cad. Demirli İş Merkezi Kadıköy - İSTANBUL",
+      url: "https://goo.gl/maps/RNiqv558Wzu3nqeR7",
+      icon: Icons.location_on,
+    ),
+  ];
 }
