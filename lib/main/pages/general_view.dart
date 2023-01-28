d40a2490e22cbd8e8e535336a2d50374225244a9
commandiron <commandiron@gmail.com>
2023-01-28 13:28:30 +0300

    .

--- lib/main/pages/general_view.dart
+++ lib/main/pages/general_view.dart
@@ -1,9 +1,7 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 import '../../model/project.dart';
-import '../widget/carousel_item.dart';
 
 class GeneralView extends StatelessWidget {
   const GeneralView({Key? key}) : super(key: key);
@@ -15,24 +13,14 @@ class GeneralView extends StatelessWidget {
       builder: (context, index) {
         return PhotoViewGalleryPageOptions(
           imageProvider: AssetImage(Project.project1.generalViewImagePaths[index]),
-          initialScale: PhotoViewComputedScale.contained * 1,
-            heroAttributes: PhotoViewHeroAttributes(tag: Project.project1.generalViewImagePaths[index])
+          initialScale: PhotoViewComputedScale.contained * 0.8,
+            heroAttributes: PhotoViewHeroAttributes(tag: Project.project1.generalViewImagePaths[index]),
+          minScale: PhotoViewComputedScale.contained * 0.8,
+          maxScale: 4,
         );
       },
-    );
-
-    CarouselSlider.builder(
-      itemCount: Project.project1.generalViewImagePaths.length,
-      itemBuilder: (context, index, realIndex) {
-        return CarouselItem(
-          imagePath: Project.project1.generalViewImagePaths[index],
-          color: Theme.of(context).colorScheme.onBackground,
-          fit: BoxFit.cover,
-        );
-      },
-      options: CarouselOptions(
-        enlargeCenterPage: true,
-          height: MediaQuery.of(context).size.height / 1.6,
+      backgroundDecoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.background
       )
     );
   }
--- lib/main/pages/plans.dart
+++ lib/main/pages/plans.dart
@@ -1,28 +1,10 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
-import '../../model/project.dart';
-import '../widget/carousel_item.dart';
 
 class Plans extends StatelessWidget {
   const Plans({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      itemCount: Project.project1.planImagePaths.length,
-      itemBuilder: (context, index, realIndex) {
-        return CarouselItem(
-          imagePath: Project.project1.planImagePaths[index],
-          color: Colors.transparent,
-          shadowColor: Colors.transparent,
-          fit: BoxFit.fitWidth,
-        );
-      },
-      options: CarouselOptions(
-        height: MediaQuery.of(context).size.height / 1.6,
-        enlargeCenterPage: true,
-      )
-    );
+    return Container();
   }
 }
