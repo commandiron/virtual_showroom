1381129d911c31ce5e581f10c31e3d961dd87566
commandiron <commandiron@gmail.com>
2023-01-29 14:09:57 +0300

    .

--- lib/main/pages/project_info.dart
+++ lib/main/pages/project_info.dart
@@ -13,32 +13,50 @@ class ProjectInfo extends StatelessWidget {
     final estimatedTotalDuration = Project.project1.estimatedFinishDate.difference(Project.project1.startDate).inDays;
     final passedTime = DateTime.now().difference(Project.project1.startDate).inDays;
 
-    return Column(
-      children: [
-        const Text("Tamamlanma tarihi: 10/2023"),
-        AppSpace.verticalL!,
-        SizedBox(
-          width: 128,
-          child: CircleProgressBar(
-            backgroundColor:  Theme.of(context).colorScheme.onBackground.withOpacity(0.35),
-            foregroundColor: Theme.of(context).colorScheme.primary,
-            animationDuration: Duration(milliseconds: 2000),
-            value: passedTime / estimatedTotalDuration,
-            child: Center(
-              child: AnimatedCount(
-                count: (passedTime / estimatedTotalDuration) * 100,
-                fractionDigits: 0,
-                unit: '%',
-                duration: Duration(milliseconds: 2000),
-                curve: Curves.fastOutSlowIn,
-                style: AppTextStyle.b1!,
+    return SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            SizedBox(
+              width: 200,
+              child: CircleProgressBar(
+                backgroundColor:  Theme.of(context).colorScheme.onBackground.withOpacity(0.85),
+                foregroundColor: Theme.of(context).colorScheme.primary,
+                animationDuration: Duration(milliseconds: 2000),
+                value: passedTime / estimatedTotalDuration,
+                strokeWidth: 16,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Tamamlanma Yüzdesi:",
+                        textAlign: TextAlign.center,
+                        style: AppTextStyle.b2!,
+                      ),
+                      AnimatedCount(
+                        count: (passedTime / estimatedTotalDuration) * 100,
+                        fractionDigits: 0,
+                        unit: '%',
+                        duration: Duration(milliseconds: 2000),
+                        curve: Curves.fastOutSlowIn,
+                        style: AppTextStyle.b1!,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
+            AppSpace.verticalL!,
+            Text("Bitiş tarihi: 10/2023", style: AppTextStyle.b2!,),
+            AppSpace.verticalL!,
+            SizedBox(
+              height: 1000,
+            )
+          ],
         ),
-        AppSpace.verticalL!,
-        Text("Proje Bilgileri", style: AppTextStyle.b1!,),
-      ],
+      ),
     );
   }
 }
