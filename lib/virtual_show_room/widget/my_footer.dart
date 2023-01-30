e30f8d474378beef30148ef43328be59bae228ea
commandiron <commandiron@gmail.com>
2023-01-30 12:51:03 +0300

    .

--- lib/virtual_show_room/widget/my_footer.dart
+++ lib/virtual_show_room/widget/my_footer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:virtual_showroom/configs/app_padding.dart';
 import 'package:virtual_showroom/configs/app_text_style.dart';
+import 'package:virtual_showroom/helper/url_launcher.dart';
 
 class MyFooter extends StatelessWidget {
   const MyFooter({Key? key}) : super(key: key);
@@ -14,27 +15,32 @@ class MyFooter extends StatelessWidget {
       padding: AppPadding.allXS,
       alignment: Alignment.center,
       child: FittedBox(
-        child: Row(
-          children: [
-            Text(
-              "from ",
-              style: AppTextStyle.b1?.copyWith(
-                color: Theme.of(context).colorScheme.onSecondaryContainer.withOpacity(0.3)
+        child: InkWell(
+          onTap: () {
+            openURL("https://commandiron.github.io");
+          },
+          child: Row(
+            children: [
+              Text(
+                "from ",
+                style: AppTextStyle.b1?.copyWith(
+                  color: Theme.of(context).colorScheme.onSecondaryContainer.withOpacity(0.3)
+                ),
               ),
-            ),
-            Text(
-              "DEMIRLI",
-              style: AppTextStyle.b1?.copyWith(
-                color: Theme.of(context).colorScheme.onSecondaryContainer
+              Text(
+                "DEMIRLI",
+                style: AppTextStyle.b1?.copyWith(
+                  color: Theme.of(context).colorScheme.onSecondaryContainer
+                ),
               ),
-            ),
-            Text(
-              "tech",
-              style: AppTextStyle.b1?.copyWith(
-                color: Color(0xffF48B0B)
+              Text(
+                "tech",
+                style: AppTextStyle.b1?.copyWith(
+                  color: Color(0xffF48B0B)
+                ),
               ),
-            ),
-          ]
+            ]
+          ),
         ),
       ),
     );
