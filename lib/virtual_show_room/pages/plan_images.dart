1200c1575055d666321f037ce10a8acfa5879f30
Emir Demirli <emirdemirli@gmail.com>
2023-02-06 20:35:05 +0300

    .

--- lib/virtual_show_room/pages/plan_images.dart
+++ lib/virtual_show_room/pages/plan_images.dart
@@ -1,8 +1,9 @@
-import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view_gallery.dart';
+import 'package:provider/provider.dart';
 import 'package:virtual_showroom/configs/app_padding.dart';
 import 'package:virtual_showroom/model/project.dart';
+import 'package:virtual_showroom/provider/app_state_provider.dart';
 import 'package:virtual_showroom/virtual_show_room/widget/app_dots_indicator.dart';
 
 class PlanImagesPage extends StatefulWidget {
@@ -17,41 +18,7 @@ class PlanImagesPage extends StatefulWidget {
 }
 
 class _PlanImagesPageState extends State<PlanImagesPage> {
-
   Apartment? _selectedApartment;
-
-  @override
-  Widget build(BuildContext context) {
-    return _selectedApartment == null ? SingleChildScrollView(
-      child: Column(
-        children: widget.apartments.map(
-          (apartment) => ApartmentItem(apartment: apartment,)
-        ).toList(),
-      ),
-    ) : Center(
-      child: PhotoViewGallery.builder(
-        itemCount: _selectedApartment!.imagePaths.length,
-        builder: (context, index) {
-          return PhotoViewGalleryPageOptions(
-            imageProvider: AssetImage(_selectedApartment!.imagePaths[index]),
-          );
-        },
-      ),
-    );
-  }
-}
-
-
-class ApartmentItem extends StatefulWidget {
-  const ApartmentItem({required this.apartment, Key? key}) : super(key: key);
-
-  final Apartment apartment;
-
-  @override
-  State<ApartmentItem> createState() => _ApartmentItemState();
-}
-
-class _ApartmentItemState extends State<ApartmentItem> {
 
   double _currentIndex = 0;
   final PageController _pageController = PageController();
@@ -59,7 +26,82 @@ class _ApartmentItemState extends State<ApartmentItem> {
   @override
   void initState() {
     _pageController.addListener(() {
-      setState((){
+      setState(() {
+        _currentIndex = _pageController.page ?? 0;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _selectedApartment == null
+        ? SingleChildScrollView(
+            child: Column(
+              children: widget.apartments
+                  .map((apartment) => ApartmentItem(
+                      apartment: apartment,
+                      onSelect: () {
+                        setState(() {
+                          _selectedApartment = apartment;
+                          Provider.of<AppStateProvider>(context, listen: false)
+                              .setScreenStateExpanded(isAnimated: true);
+                        });
+                      }))
+                  .toList(),
+            ),
+          )
+        : Center(
+            child: Stack(
+              children: [
+                PhotoViewGallery.builder(
+                  pageController: _pageController,
+                  itemCount: _selectedApartment!.imagePaths.length,
+                  builder: (context, index) {
+                    return PhotoViewGalleryPageOptions(
+                      imageProvider:AssetImage(_selectedApartment!.imagePaths[index]),
+                    );
+                  },
+                ),
+                if (_selectedApartment!.imagePaths.length > 1)
+                  AppDotsIndicator(
+                      dotsCount: _selectedApartment!.imagePaths.length,
+                      position: _currentIndex),
+                BackButton(
+                  onPressed: () {
+                    setState(() {
+                      _selectedApartment = null;
+                      Provider.of<AppStateProvider>(context, listen: false)
+                          .setScreenStateCollapsed();
+                    });
+                  },
+                )
+              ],
+            ),
+          );
+  }
+}
+
+class ApartmentItem extends StatefulWidget {
+  const ApartmentItem(
+      {required this.apartment, required this.onSelect, Key? key})
+      : super(key: key);
+
+  final Apartment apartment;
+  final Function onSelect;
+
+  @override
+  State<ApartmentItem> createState() => _ApartmentItemState();
+}
+
+class _ApartmentItemState extends State<ApartmentItem> {
+  double _currentIndex = 0;
+  final PageController _pageController = PageController();
+
+  @override
+  void initState() {
+    _pageController.addListener(() {
+      setState(() {
         _currentIndex = _pageController.page ?? 0;
       });
     });
@@ -72,13 +114,12 @@ class _ApartmentItemState extends State<ApartmentItem> {
       padding: AppPadding.allM!,
       child: Card(
         clipBehavior: Clip.hardEdge,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16)
-        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         borderOnForeground: true,
         child: Column(
           children: [
             Stack(
+              alignment: Alignment.bottomCenter,
               children: [
                 Container(
                     height: 200,
@@ -88,15 +129,18 @@ class _ApartmentItemState extends State<ApartmentItem> {
                       scrollDirection: Axis.horizontal,
                       itemCount: widget.apartment.imagePaths.length,
                       itemBuilder: (context, index) {
-                        return Image.asset(widget.apartment.imagePaths[index]);
+                        return InkWell(
+                            onDoubleTap: () {
+                              widget.onSelect();
+                            },
+                            child: Image.asset(
+                                widget.apartment.imagePaths[index]));
                       },
-                    )
-                ),
-                if(widget.apartment.imagePaths.length > 1)
+                    )),
+                if (widget.apartment.imagePaths.length > 1)
                   AppDotsIndicator(
-                    dotsCount: widget.apartment.imagePaths.length,
-                    position: _currentIndex
-                  )
+                      dotsCount: widget.apartment.imagePaths.length,
+                      position: _currentIndex)
               ],
             ),
             SizedBox(
@@ -109,5 +153,3 @@ class _ApartmentItemState extends State<ApartmentItem> {
     );
   }
 }
-
-
