9f363ba417dca5f7817fcf9835e3afc4b24a14ca
commandiron <commandiron@gmail.com>
2023-02-08 18:00:18 +0300

    .

--- lib/virtual_show_room/pages/plans/widget/lottie_animation.dart
+++ lib/virtual_show_room/pages/plans/widget/lottie_animation.dart
@@ -1,28 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class LottieAnimation extends StatefulWidget {
+class LottieAnimation extends StatelessWidget {
   const LottieAnimation({Key? key}) : super(key: key);
-
-  @override
-  State<LottieAnimation> createState() => _LottieAnimationState();
-}
-
-class _LottieAnimationState extends State<LottieAnimation> with TickerProviderStateMixin  {
-
-  late final AnimationController _animationController;
-
-  @override
-  void initState() {
-    _animationController = AnimationController(vsync: this);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +10,7 @@ class _LottieAnimationState extends State<LottieAnimation> with TickerProviderSt
       child: Center(
         child: Lottie.asset(
           "assets/lottie/zoom_in_animation_lottie.json",
-          controller: _animationController,
-          onLoaded: (composition) {
-            _animationController
-              ..duration = composition.duration
-              ..forward();
-          },
+          repeat: false,
         ),
       ),
     );
